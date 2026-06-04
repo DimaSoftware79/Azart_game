7be3fe55c2cd86530105a7553d734b778dd3359a
DIMAtestGITHUB <tgithub986@gmail.com0>
2026-06-04 04:49:09 +0300

    add testing fils

---
+++ AzartClass.py
@@ -0,0 +1,8 @@
+class AzartClass:
+	def __init__(self):
+		self.test = 0
+	
+	def main(self):
+		if self.test == 0:
+			self.test +=1
+			return "test = 1"
--- main_tset.py
+++ main_tset.py
@@ -4,12 +4,35 @@ def main():
 	DifficultyFusion = []
 	while True:
 		taking = False
+		c = True 
 		mony = 100
 		Azart = AzartClass()
 		difficulty = input("Enter what difficult do you want: ")
 		while True:
-			mony_str = "$"+ str(mony)						
-			if difficulty == "easi":
+			print(f"var = {difficulty}, list = {DifficultyFusion}")
+			mony_str = "$"+ str(mony)
+			if difficulty == "cheats":
+				print("WARNING: Add in DifficultyFusion cheats")
+				DifficultyFusion.append(difficulty)
+				print(f"DifficultyFusion = {DifficultyFusion}")
+				
+				_= difficulty
+				#print(f"1.) {difficulty}")
+				difficulty = ""
+				#print(f"2.) {difficulty}")
+				#print(f"3.) _ = {_}")
+				
+				#taking = True
+			#elif c:
+				#print(f"DifficultyFusion + c = {DifficultyFusion}")
+				#c = False
+				#break
+				
+			elif "cheats" in DifficultyFusion:
+				print("not implemeted yet!")
+				DifficultyFusion =[]
+			
+			elif difficulty == "easi":
 				secret_number  = randint(1, 3) 
 				taking = True
 			elif difficulty == "hard":
@@ -22,6 +45,12 @@ def main():
 				print("choose difficulty") 
 				break
 			
+			
+				 
+
+
+
+
 			if taking:
 				print(f"balanse:{mony_str}") 
 				number =input("enter yor nember:")
